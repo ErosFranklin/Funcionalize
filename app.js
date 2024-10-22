25fa279a66785f5c199f5cf2701c90512b217e1f
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2024-10-22 14:47:16 -0300

    add mongoBD

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+//Coloquem comentarios que mlk Esdras gosta
+require('dotenv').config();
 
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Codigo para verificar a conexao com o MONGO
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+
+// Middleware para JSON
+app.use(express.json());
+
+// Rotas 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('API rodando!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
