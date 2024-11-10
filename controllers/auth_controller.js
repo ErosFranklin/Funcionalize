255019bd984544c5548bab3813fa2fa739dfbaaf
Kaio Vinícius Cordeiro Batista <kaiovbatista19@gmail.com>
2024-11-10 17:25:31 -0300

    Criação do logger e adição dele nos controllers

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const Token = require('../models/token.js')
 const User = require('../models/user.js');   
 const bcrypt = require('bcrypt');
+const logger = require('../logger/logger.js');
 
 const loginController = async (data) => {
    
@@ -11,19 +12,23 @@ const loginController = async (data) => {
         
         const user = await User.findOne({ email });
         if (!user) {
+            logger.warn(`Usuário com credencial ${email} não encontrado.`);
             return { error: 'Usuário não existe', statusCode: 401 };
         }
         
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
+            logger.warn(`Senha inválida para o usuário com credencial ${email}.`);
             return { error: 'Senha inválida', statusCode: 401 };
         }
 
         const tokenPayload = {
             id: user._id,
             email: user.email,
+            name: user.name,
             type: "access", 
         };
+        
         const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         const newToken = new Token({
@@ -35,9 +40,10 @@ const loginController = async (data) => {
 
         await newToken.save();
 
+        logger.info(`Sucesso no login para usuário com credencial ${email}.`);
         return { message: 'Sucesso no Login', token, user, statusCode: 200 };
     } catch (err) {
-        console.error('Error in loginController:', err);
+        logger.error(`Error in loginController: ${err.message}`);
         return { error: 'Internal server error', statusCode: 500 };
     }
 };
@@ -45,8 +51,11 @@ const getUserData = async (user) => {
     try {
         const userData = await User.findById(user.id).select('-password'); 
         if (!userData) {
+            logger.warn(`Usuário não encontrado ao buscar seus dados.`);
             return { error: 'Usuário não encontrado' }; 
         }
+
+        logger.info(`Usuário encontrado ao buscar seus dados.`);
         return { user: userData }; 
     } catch (error) {
         throw new Error('Erro ao buscar dados do usuário'); 
--- controllers/employee_controller.js
+++ controllers/employee_controller.js
@@ -1,96 +1,134 @@
 const Employee = require('../models/employee');
+const logger = require('../logger/logger.js');
 
 // Criar um funcionário
 exports.createEmployee = async (req, res) => {
-    const employee = new Employee({
-      ...req.body,
-      user: req.user.id
-    });
-    
-    try {
-      const savedEmployee = await employee.save();
-      res.status(201).json(savedEmployee);
-    } catch (err) {
-      res.status(400).json({ message: 'Erro ao criar funcionário: ' + err.message });
-    }
-  };
- // Buscar um funcionário 
-exports.getEmployeeById = async (req, res) => {
-  const { id } = req.params; 
 
-  if (!req.user || !req.user.id) {
-      return res.status(400).json({ message: 'Usuário não autenticado' });
-  }
+  logger.info("Criando um novo funcionário.");
+
+  const employee = new Employee({
+    ...req.body,
+    user: req.user.id
+  });
 
   try {
-      const employee = await Employee.findOne({ _id: id, user: req.user.id });
-
-      if (!employee) {
-          return res.status(404).json({ message: 'Funcionário não encontrado ou você não tem permissão' });
-      }
-      
-      res.json(employee);
+    const savedEmployee = await employee.save();
+    logger.info(`O funcionário de nome ${savedEmployee.name} foi criado com sucesso e vinculado à empresa ${req.user.name}.`);
+    res.status(201).json(savedEmployee);
   } catch (err) {
-      res.status(500).json({ message: 'Erro ao buscar funcionário: ' + err.message });
+    logger.error(`Erro ao criar funcionário: ${err.message}`);
+    res.status(400).json({ message: 'Erro ao criar funcionário: ' + err.message });
   }
 };
-  // Ler todos os funcionários
-  exports.getAllEmployees = async (req, res) => {
-    try {
-      const employees = await Employee.find({ user: req.user.id });
-      res.json(employees);
-    } catch (err) {
-      res.status(500).json({ message: 'Erro ao buscar funcionários: ' + err.message });
+
+// Buscar um funcionário pelo id
+exports.getEmployeeById = async (req, res) => {
+  const id = req.params.id;
+
+  if (!req.user || !req.user.id) {
+    logger.warn(`Tentativa de acesso sem autenticação para buscar funcionário com ID: ${id}`)
+    return res.status(401).json({ message: 'Usuário não autenticado' });
+  }
+
+  logger.info(`Buscando funcionário com ID: ${id}`);
+
+  try {
+    const employee = await Employee.findOne({ _id: id, user: req.user.id });
+
+    if (!employee) {
+      logger.warn(`Funcionário com ID ${id} não encontrado ou sem permissão de acesso pelo usuário logado`);
+      return res.status(404).json({ message: 'Funcionário não encontrado ou você não tem permissão para buscá-lo' });
     }
-  };
-  
-  // Atualizar um funcionário
+
+    logger.info(`Funcionário encontrado: (nome) ${employee.name}; (idade) ${employee.age}; (salário) ${employee.salary}; (departamento) ${employee.department}`);
+    res.status(200).json(employee);
+  } catch (err) {
+    logger.error(`Erro ao buscar funcionário com ID ${id}: ${err.message}`);
+    res.status(500).json({ message: 'Erro ao buscar funcionário: ' + err.message });
+  }
+};
+
+// Ler todos os funcionários
+exports.getAllEmployees = async (req, res) => {
+  logger.info(`Listando todos os funcionários da empresa ${req.user.name}.`);
+
+  try {
+    const employees = await Employee.find({ user: req.user.id });
+    logger.info(`Funcionários encontrados.`);
+    res.status(200).json(employees);
+  } catch (err) {
+    logger.error(`Erro ao buscar funcionários: ${err.message}`);
+    res.status(500).json({ message: 'Erro ao buscar funcionários: ' + err.message });
+  }
+};
+
+// Atualizar um funcionário
 exports.updateEmployee = async (req, res) => {
+
+  const idOutdatedEmployee = req.params.id;
+
+  logger.info(`Atualizando o funcionário do ID ${idOutdatedEmployee}.`);
+
   try {
     const updateData = {
       name: req.body.name,
-      age: req.body.age,
-      salary: req.body.salary,
+      age: parseInt(req.body.age),
+      salary: parseFloat(req.body.salary),
       department: req.body.department
     };
 
+    // Atualizando o funcionário no banco
     const employee = await Employee.findOneAndUpdate(
-      { _id: req.params.id, user: req.user.id },
+      { _id: idOutdatedEmployee, user: req.user.id },
       updateData,
       { new: true } // Retorna o documento atualizado
     );
 
-    if (!employee) {
-      return res.status(404).json({ message: 'Funcionário não encontrado ou você não tem permissão' });
+    if (!employee){
+      logger.warn(`Funcionário com ID ${idOutdatedEmployee} não encontrado ou sem permissão de acesso pelo usuário com ID ${req.user.id}`);
+      return res.status(404).json({ message: 'Funcionário não encontrado ou você não tem permissão para alterá-lo' });
     }
 
-    res.json(employee);
+    logger.info(`Funcionário com ID ${employee._id} atualizado com sucesso.`);
+    res.status(200).json(employee);
   } catch (err) {
+    logger.error(`Erro ao atualizar funcionário com ID ${idOutdatedEmployee}: ${err.message}`);
     res.status(400).json({ message: 'Erro ao atualizar funcionário: ' + err.message });
   }
 };
-  
-  // Deletar um funcionário
-  exports.deleteEmployee = async (req, res) => {
-    try {
-      const employee = await Employee.findOneAndDelete({ _id: req.params.id, user: req.user.id }); 
-  
-      if (!employee) {
-        return res.status(404).json({ message: 'Funcionário não encontrado ou você não tem permissão' });
-      }
-  
-      res.status(204).send();
-    } catch (err) {
-      res.status(500).json({ message: 'Erro ao deletar funcionário: ' + err.message });
+
+// Deletar um funcionário
+exports.deleteEmployee = async (req, res) => {
+  const idDeletedEmployee = req.params.id;
+
+  logger.info(`Deletando o funcionário do ID ${idDeletedEmployee}.`);
+
+  try {
+    const employee = await Employee.findOneAndDelete({ _id: idDeletedEmployee, user: req.user.id });
+
+    if (!employee) {
+      logger.warn(`Funcionário com ID ${idDeletedEmployee} não encontrado ou sem permissão de acesso pelo usuário com ID ${req.user.id}`);
+      return res.status(404).json({ message: 'Funcionário não encontrado ou você não tem permissão para deletá-lo' });
     }
-  };
-  
-  // Deletar todos os funcionários do usuário autenticado
-  exports.deleteAllEmployees = async (req, res) => {
-    try {
-      await Employee.deleteMany({ user: req.user.id }); // Deleta todos os funcionários do usuário
-      res.status(204).send(); 
-    } catch (err) {
-      res.status(500).json({ message: 'Erro ao deletar todos os funcionários: ' + err.message });
-    }
-  };
+
+    logger.info(`Funcionário com ID ${employee._id} deletado com sucesso.`);
+    res.status(204).send();
+  } catch (err) {
+    logger.error(`Erro ao deletar funcionário com ID ${idDeletedEmployee}: ${err.message}`);
+    res.status(500).json({ message: 'Erro ao deletar funcionário: ' + err.message });
+  }
+};
+
+// Deletar todos os funcionários do usuário autenticado
+exports.deleteAllEmployees = async (req, res) => {
+  logger.info(`Deletando todos os funcionário da empresa ${req.user.name}.`);
+
+  try {
+    await Employee.deleteMany({ user: req.user.id }); // Deleta todos os funcionários do usuário
+    logger.info(`Funcionários da empresa ${req.user.name} deletados com sucesso.`);
+    res.status(204).send();
+  } catch (err) {
+    logger.error(`Erro ao deletar os funcionários da empresa ${req.user.name}: ${err.message}`);
+    res.status(500).json({ message: 'Erro ao deletar todos os funcionários: ' + err.message });
+  }
+};
